084bd8a11e7cd5495ac2f7624fe2de8eb487cb7b
lovehzj <1922919664@qq.com>
2022-01-14 20:00:19 +0800

    Release docker image binary

--- docker-build/scripts/start-daemon.sh
+++ docker-build/scripts/start-daemon.sh
@@ -2,6 +2,6 @@
 
 export REMOTE_MODE=false
 
-mkdir $MYSQL_HOME/logs
+mkdir -p $MYSQL_HOME/logs
 nohup $MYSQL_HOME/mate/mysql_mate >>$MYSQL_HOME/logs/mysql_mate.stdout.log 2>>$MYSQL_HOME/mysql_mate.stderr.log
 
